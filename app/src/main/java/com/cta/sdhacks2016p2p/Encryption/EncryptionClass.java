d548bb7b503402fe7365a799389dd9cf3a0035d5
lepatrick714 <leduylamc@gmail.com>
2016-10-01 21:42:48 -0700

    Private and public key works

--- app/src/main/java/com/cta/sdhacks2016p2p/Encryption/EncryptionClass.java
+++ app/src/main/java/com/cta/sdhacks2016p2p/Encryption/EncryptionClass.java
@@ -2,6 +2,7 @@ import java.lang.*;
 import java.math.*;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
 
 public class EncryptionClass {
 
@@ -26,6 +27,9 @@ public class EncryptionClass {
 
         private BigInteger e; //1 < e < phi(n) and gcd(phi(n),e) = 1
         private BigInteger d; //1 < d < phi(n), e * d = 1 % phi(n)
+
+        private ArrayList<BigInteger>publicTemp;
+        private ArrayList<BigInteger>privateTemp;
 
         private BigInteger randomWithinRange(int min, int max) {
             BigInteger range = BigInteger.valueOf((max-min)+1);
@@ -57,12 +61,13 @@ public class EncryptionClass {
             cArray[kSize-1] = BigInteger.valueOf(1);
 
             // 2. compute b1
+            sumBi = BigInteger.valueOf(0);
             for (int i=0; i<kSize; i++) {
                 BigInteger temp = commputeBi(BigInteger.valueOf(i));
                 bArray[i] = temp;
                 sumBi = temp.add(sumBi);
             }
-
+;
             // 3. randomly choose a modulus
             do {
                 M = randomWithinRange(1, Integer.MAX_VALUE);
@@ -73,30 +78,29 @@ public class EncryptionClass {
             for (int i=0; i<kSize; i++) {
                 aArray[i] = bArray[i].multiply(w).mod(M);
             }
-
             do {
-                do {
-                    p = randomWithinRange(0, Integer.MAX_VALUE);
-                } while (p.isProbablePrime(1));
-                do {
-                    q = randomWithinRange(0, Integer.MAX_VALUE);
-                } while (q.isProbablePrime(1));
+                Random rnd1 = new Random();
+                p = BigInteger.probablePrime(100, rnd1);
+
+                Random rnd2 = new Random();
+                q = BigInteger.probablePrime(100, rnd2);
             } while (p.subtract(BigInteger.valueOf(1)).divide(BigInteger.valueOf(2)).isProbablePrime(1) &&
-            q.subtract(BigInteger.valueOf(1)).divide(BigInteger.valueOf(2)).isProbablePrime(1));
+                    q.subtract(BigInteger.valueOf(1)).divide(BigInteger.valueOf(2)).isProbablePrime(1));
 
             n = p.multiply(q);
             phiN = (p.subtract(BigInteger.valueOf(1)).multiply(q.subtract(BigInteger.valueOf(1))));
 
             do {
-                 e = randomWithinRange(0, Integer.MAX_VALUE);
-            } while (e.compareTo(BigInteger.valueOf(1)) != 1 && e.compareTo(phiN) != 1 && phiN.gcd(e) !=  BigInteger.valueOf(1));
-
+                 Random rnd = new Random();
+                 e = BigInteger.probablePrime(100, rnd);
+            } while (e.compareTo(BigInteger.valueOf(1)) > 0 && e.compareTo(phiN) < 0 && phiN.gcd(e) ==  BigInteger.valueOf(1));
             ExtendedEuclid();
 
-            ArrayList<BigInteger>privateTemp = new ArrayList<BigInteger>(Arrays.asList(aArray));
+            privateTemp = new ArrayList<BigInteger>(Arrays.asList(aArray));
             privateTemp.add(n);
             privateTemp.add(e);
-            ArrayList<BigInteger>publicTemp = new ArrayList<BigInteger>(Arrays.asList(bArray));
+
+            publicTemp = new ArrayList<BigInteger>(Arrays.asList(bArray));
             publicTemp.add(p);
             publicTemp.add(q);
             publicTemp.add(w);
@@ -108,9 +112,20 @@ public class EncryptionClass {
 
         //e.d = 1 mod phi(N)
         public void ExtendedEuclid() {
+            System.out.println("Running ExtendedEuclid");
             do {
                 d = (BigInteger.valueOf(1).mod(phiN)).divide(e);
-            } while(d.compareTo(BigInteger.valueOf(1)) != 1 && d.compareTo(phiN) != 1);
+            } while(d.compareTo(BigInteger.valueOf(1)) > 0 && e.compareTo(phiN) < 0);
+        }
+
+        public void printKeys() {
+            System.out.println("Printing the private key");
+            for(BigInteger s : privateTemp)
+                System.out.println(s);
+
+            System.out.println("Printing the public key");
+            for(BigInteger s : publicTemp)
+                System.out.println(s);
         }
     }
 
